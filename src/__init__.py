449a4dd241b1182bb7fd650a90ef238314e8e1dd
astivard <mdravitsa2@gmail.com>
2023-02-23 23:35:05 +0300

    Test imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
+from views import bp, page_not_found
+from models import db
 
 
 def create_app():
     app = Flask(__name__)
-    from src import views, models
     app.config.from_pyfile('config.py')
-    app.register_blueprint(views.bp)
-    app.register_error_handler(404, views.page_not_found)
-    models.db.init_app(app)
+    app.register_blueprint(bp)
+    app.register_error_handler(404, page_not_found)
+    db.init_app(app)
     with app.app_context():
-        models.db.create_all()
+        db.create_all()
     return app
