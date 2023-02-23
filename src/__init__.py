f624cc917ced53de7476a18f6d1070819a355a1c
astivard <mdravitsa2@gmail.com>
2023-02-23 23:26:35 +0300

    Test another structure 2

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    from . import views, models
+    from src import views, models
     app.config.from_pyfile('config.py')
     app.register_blueprint(views.bp)
     app.register_error_handler(404, views.page_not_found)
